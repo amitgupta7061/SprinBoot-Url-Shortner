bea5b03543a1fd35cf2840ae8446eb052a16f958
Amit <amitgupta561710@gmail.com>
2025-07-22 02:01:55 +0530

    first-commit

---
+++ src/main/java/com/example/url_sortner/UrlSortnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.url_sortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlSortnerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/url_sortner/models/User.java
@@ -0,0 +1,20 @@
+package com.example.url_sortner.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String role = "USER";
+
+}
